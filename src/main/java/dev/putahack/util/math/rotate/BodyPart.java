e063032b9c6a4dfb87d2898f343d6462cc8595d3
Sxmurai <gavrw0607@gmail.com>
2023-06-07 19:49:09 -0500

    entity rotation calc

--- src/main/java/dev/putahack/util/math/rotate/BodyPart.java
+++ src/main/java/dev/putahack/util/math/rotate/BodyPart.java
@@ -1,12 +1,27 @@
 package dev.putahack.util.math.rotate;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+
+import java.util.function.Function;
+
 /**
  * @author aesthetical
  * @since 06/07/23
  */
 public enum BodyPart {
-    HEAD,
-    TORSO,
-    LEGS,
-    FEET
+    HEAD(Entity::getEyeHeight),
+    TORSO((e) -> e.getEyeHeight() - 0.2f),
+    LEGS((e) -> e.getEyeHeight() / 2.0f),
+    FEET((e) -> 0.0f);
+
+    private final Function<EntityLivingBase, Float> function;
+
+    BodyPart(Function<EntityLivingBase, Float> function) {
+        this.function = function;
+    }
+
+    public float apply(EntityLivingBase t) {
+        return function.apply(t);
+    }
 }
--- src/main/java/dev/putahack/util/math/rotate/RotationUtils.java
+++ src/main/java/dev/putahack/util/math/rotate/RotationUtils.java
@@ -2,6 +2,8 @@ package dev.putahack.util.math.rotate;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 
 /**
  * @author aesthetical
@@ -9,6 +11,9 @@ import net.minecraft.entity.EntityLivingBase;
  */
 public class RotationUtils {
 
+    /**
+     * The minecraft game instance
+     */
     private static final Minecraft mc = Minecraft.getMinecraft();
 
     /**
@@ -18,6 +23,18 @@ public class RotationUtils {
      * @return the calculated rotations
      */
     public static float[] entity(EntityLivingBase target, BodyPart bodyPart) {
-        return new float[2];
+        Vec3d eyeVec = mc.player.getPositionEyes(1.0f);
+        Vec3d targetEyeVec = target.getPositionVector().add(
+                0.0, bodyPart.apply(target), 0.0);
+
+        double diffX = targetEyeVec.x - eyeVec.x;
+        double diffZ = targetEyeVec.z - eyeVec.z;
+
+        float yaw = (float) -(Math.toDegrees(Math.atan2(diffX, diffZ)));
+        float pitch = (float) (-Math.toDegrees(Math.atan2(
+                targetEyeVec.y - eyeVec.y, Math.hypot(diffX, diffZ))));
+
+        return new float[] {
+                yaw, MathHelper.clamp(pitch, -90.0f, 90.0f) };
     }
 }
