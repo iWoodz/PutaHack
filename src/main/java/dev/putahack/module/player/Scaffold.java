51eb21cd5a9cceb7849cd9af309c0b87df9994ef
Sxmurai <gavrw0607@gmail.com>
2023-06-09 12:03:39 -0500

    swap options to scaffold

--- src/main/java/dev/putahack/module/player/Scaffold.java
+++ src/main/java/dev/putahack/module/player/Scaffold.java
@@ -7,6 +7,7 @@ import dev.putahack.module.Module;
 import dev.putahack.module.ModuleCategory;
 import dev.putahack.setting.Setting;
 import dev.putahack.util.math.rotate.RotationUtils;
+import dev.putahack.util.timing.Timer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.client.CPacketAnimation;
@@ -22,13 +23,18 @@ import net.minecraft.util.math.Vec3d;
  * @since 06/07/23
  */
 public class Scaffold extends Module {
+    private final Setting<Double> delay = new Setting<>(50.0, 0.01, 0.0, 500.0, "Delay");
     private final Setting<Boolean> rotate = new Setting<>(true, "Rotate");
+    private final Setting<Swap> swap = new Setting<>(Swap.LITE_SPOOF, "Swap");
     private final Setting<Boolean> swing = new Setting<>(true, "Swing");
     private final Setting<Boolean> tower = new Setting<>(true, "Tower");
 
+    private final Timer placeTimer = new Timer();
     private float[] rotations;
     private BlockPos blockPos;
     private EnumFacing enumFacing;
+
+    private int oldSlot = -1;
 
     public Scaffold() {
         super("Scaffold", "Places blocks rapidly at your feet", ModuleCategory.PLAYER);
@@ -43,8 +49,17 @@ public class Scaffold extends Module {
         enumFacing = null;
 
         if (mc.player != null) {
-            PutaHack.get().getInventory().sync();
+            if (swap.getValue() == Swap.HOLD) {
+                if (oldSlot != -1) {
+                    mc.player.inventory.currentItem = oldSlot;
+                }
+
+            } else {
+                PutaHack.get().getInventory().sync();
+            }
         }
+
+        oldSlot = -1;
     }
 
     @Listener
@@ -73,29 +88,53 @@ public class Scaffold extends Module {
 
         if (slot == -1) return;
 
-        if (PutaHack.get().getInventory().getServerSlot() != slot) {
-            mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
-        }
+        if (placeTimer.hasPassed(delay.getValue().longValue(), false)) {
 
-        EnumActionResult result = mc.playerController.processRightClickBlock(mc.player,
-                mc.world,
-                blockPos,
-                enumFacing,
-                getHitVec(),
-                EnumHand.MAIN_HAND);
-        if (result == EnumActionResult.SUCCESS) {
-            if (swing.getValue()) {
-                mc.player.swingArm(EnumHand.MAIN_HAND);
-            } else {
-                mc.player.connection.sendPacket(
-                        new CPacketAnimation(EnumHand.MAIN_HAND));
+            switch (swap.getValue()) {
+                case MANUAL:
+                    if (!(mc.player.inventory.getCurrentItem().getItem() instanceof ItemBlock)) {
+                        return;
+                    }
+                    break;
+
+                case LITE_SPOOF:
+                case LITE:
+                    if (PutaHack.get().getInventory().getServerSlot() != slot) {
+                        mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
+                    }
+                    break;
+
+                case HOLD:
+                    mc.player.inventory.currentItem = slot;
+                    break;
             }
 
-            if (tower.getValue() && mc.gameSettings.keyBindJump.isKeyDown()) {
-                if (mc.player.onGround && mc.player.motionY < 0.1) {
-                    mc.player.motionY = 0.41999998688697815;
-                } else if (mc.player.motionY <= 0.16477328182606651) {
-                    mc.player.motionY = 0.41999998688697815;
+            EnumActionResult result = mc.playerController.processRightClickBlock(mc.player,
+                    mc.world,
+                    blockPos,
+                    enumFacing,
+                    getHitVec(),
+                    EnumHand.MAIN_HAND);
+            if (result == EnumActionResult.SUCCESS) {
+                placeTimer.resetTime();
+
+                if (swing.getValue()) {
+                    mc.player.swingArm(EnumHand.MAIN_HAND);
+                } else {
+                    mc.player.connection.sendPacket(
+                            new CPacketAnimation(EnumHand.MAIN_HAND));
+                }
+
+                if (tower.getValue() && mc.gameSettings.keyBindJump.isKeyDown()) {
+                    if (mc.player.onGround && mc.player.motionY < 0.1) {
+                        mc.player.motionY = 0.41999998688697815;
+                    } else if (mc.player.motionY <= 0.16477328182606651) {
+                        mc.player.motionY = 0.41999998688697815;
+                    }
+                }
+
+                if (swap.getValue() == Swap.LITE) {
+                    PutaHack.get().getInventory().sync();
                 }
             }
         }
@@ -138,5 +177,9 @@ public class Scaffold extends Module {
 
     private boolean isReplaceable(BlockPos pos) {
         return mc.world.getBlockState(pos).getMaterial().isReplaceable();
+    }
+
+    public enum Swap {
+        MANUAL, LITE, LITE_SPOOF, HOLD
     }
 }
