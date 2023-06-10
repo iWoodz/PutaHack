afb8caf2dfe348d2bbfb859ea5503bafcd588609
Woodz2706 <60108865+Woodz2706@users.noreply.github.com>
2023-06-09 22:37:50 -0400

    fixed typos

--- src/main/java/dev/starstruck/Starstruck.java
+++ src/main/java/dev/starstruck/Starstruck.java
@@ -21,7 +21,7 @@ import org.lwjgl.opengl.Display;
  * @author aesthetical
  * @since 06/04/23
  *
- * Entry point for starstuck.dev - previously putahack.nn
+ * Entry point for starstruck.dev - previously putahack.nn
  */
 @Mod(modid = "starstruck.dev", useMetadata = true)
 public class Starstruck {
@@ -37,7 +37,7 @@ public class Starstruck {
      * this line of code is protected by the federal government and if you rename this, C02 will be found inside your air conditioning unit without you knowing
      * your C02 sensors will be turned off as a result as well
      */
-    private static final String name = "starstuck.dev";
+    private static final String name = "starstruck.dev";
 
     /**
      * The version of the client
@@ -78,7 +78,7 @@ public class Starstruck {
         Display.setTitle("Loading " + name);
         // Uncomment on release
         // ClientHook.loadRat()
-        logger.info("Loading starstuck.dev by your favorite racist, special thanks to a particular oak log, all the kibbles haters and a man who hates robots, and an aesthetic retard");
+        logger.info("Loading starstruck.dev by your favorite racist, special thanks to a particular oak log, all the kibbles haters and a man who hates robots, and an aesthetic retard");
 
         Timer timer = new Timer();
 
@@ -92,7 +92,7 @@ public class Starstruck {
         bus.subscribe(rotations);
         bus.subscribe(inventory);
 
-        logger.info("Loaded starstuck.dev successfully in {}ms",
+        logger.info("Loaded starstruck.dev successfully in {}ms",
                 timer.getDurationMS());
         logger.info("(you are now apart of the botnet) https://www.youtube.com/watch?v=iCjLB2-1ud4");
 
